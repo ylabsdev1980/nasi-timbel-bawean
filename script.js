2ef72d60c0424cda8aef4fc6363d8bafea6e6d01
NamaKamu <emailkamu@gmail.com>
2026-05-22 01:18:00 +0700

    update

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("./service-worker.js")
-      .then(() => console.log("Service Worker aktif"))
-      .catch(err => console.log("SW gagal:", err));
+  window.addEventListener("load", async () => {
+    try {
+      await navigator.serviceWorker.register("./service-worker.js");
+      console.log("SW aktif");
+    } catch (err) {
+      console.log("SW gagal:", err);
+    }
   });
-}console.log("Nasi Timbel Bawean UI Modern aktif 🚀");
+}
