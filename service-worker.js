b3f7c7da7de54024ae6ab50028e7849169f016a3
NamaKamu <emailkamu@gmail.com>
2026-05-22 06:04:31 +0700

    update

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,29 @@
-onst CACHE = "bawean-v3";
+const CACHE = "bawean-v4";
 
-self.addEventListener("install", () => {
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./style.css",
+        "./manifest.json",
+        "./icon-192.png",
+        "./icon-512.png"
+      ]);
+    })
+  );
   self.skipWaiting();
 });
 
-self.addEventListener("activate", () => {
-  clients.claim();
+self.addEventListener("activate", (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => {
+      return res || fetch(event.request);
+    })
+  );
 });
